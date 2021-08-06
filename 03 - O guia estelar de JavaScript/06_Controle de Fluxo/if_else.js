c91eae5d9b6289d55077d1d9eba943ef5c08ea64
Pedro Lisboa <phlisboa2000@gmail.com>
2021-08-05 21:20:58 -0300

    Finalizando

--- 03 - O guia estelar de JavaScript/06_Controle de Fluxo/if_else.js
+++ 03 - O guia estelar de JavaScript/06_Controle de Fluxo/if_else.js
@@ -1,1 +1,11 @@
-console.log('oi')
+// if ... else
+
+let temperatura = 36
+
+if (temperatura >= 37.5){
+    console.log('FEBRE ALTA')
+}else if(temperatura < 37.5 && temperatura >= 37){
+    console.log('FEBRIL')
+}else{
+    console.log('SEM FEBRE')
+}
